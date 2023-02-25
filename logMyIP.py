600ae22eaf6b09d65d3c821f8731179294590492
ristedev <47864975+ristedev@users.noreply.github.com>
2023-02-25 00:29:12 -0500

    Add timestamp to json, log & print result

--- logMyIP.py
+++ logMyIP.py
@@ -1,11 +1,16 @@
 import requests
 import datetime
+import json
 
 # Get datetime & convert it to a string
 ct = str(datetime.datetime.now())
 
-# Get IP & log to txt file
+# Get IP, add timestamp & log to txt file
 resp = requests.get('https://ipinfo.io')
 with open("log.txt", "a") as f:
-    f.write(ct + "\n")
-    f.write("IP Address: " + resp.text + "\n\n")
+    respData = resp.json()
+    respData["timestamp"] = ct
+    f.write(json.dumps(respData, indent=2))
+
+# Print data
+print(json.dumps(respData, indent=2))
