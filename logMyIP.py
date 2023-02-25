08497a794d67d8bb24076e2fe728bbd506c9a938
ristedev <47864975+ristedev@users.noreply.github.com>
2023-02-24 22:24:51 -0500

    Add timestamp & log response to txt file

--- logMyIP.py
+++ logMyIP.py
@@ -1,5 +1,12 @@
 import requests
+import datetime
 
+# Get datetime & convert it to a string
+ct = str(datetime.datetime.now())
+
+# Get IP & log to txt file
 resp = requests.get('https://ipinfo.io')
-with open("log.txt", "w") as f:
-    f.write(resp.text)
+with open("log.txt", "a") as f:
+    f.write("-------------------------")
+    f.write("\n" + ct + "\n")
+    f.write("IP Address: " + resp.text + "\n")
