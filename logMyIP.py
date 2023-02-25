ffd5363a81aaa0cbfcddcdc6799b32a98c9b1740
ristedev <47864975+ristedev@users.noreply.github.com>
2023-02-24 23:12:42 -0500

    Remove dashed line; use line break as separator

--- logMyIP.py
+++ logMyIP.py
@@ -7,6 +7,5 @@ ct = str(datetime.datetime.now())
 # Get IP & log to txt file
 resp = requests.get('https://ipinfo.io')
 with open("log.txt", "a") as f:
-    f.write("-------------------------")
-    f.write("\n" + ct + "\n")
-    f.write("IP Address: " + resp.text + "\n")
+    f.write(ct + "\n")
+    f.write("IP Address: " + resp.text + "\n\n")
